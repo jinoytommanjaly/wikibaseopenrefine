e2205fb5c6e725867ca334dda84115066c6537c4
Gnoeee <38123141+jinoytommanjaly@users.noreply.github.com>
2024-08-19 13:09:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 """
 This file defines a few constants which configure
 which Wikibase instance and which property/item ids
-should be used
+should be used.
 """
 
 # Endpoint of the MediaWiki API of the Wikibase instance
@@ -10,8 +10,9 @@ mediawiki_api_endpoint = 'https://opendatakerala.wikibase.cloud/w/api.php'
 
 # SPARQL endpoint
 wikibase_sparql_endpoint = 'https://opendatakerala.wikibase.cloud/query/sparql'
+
 # Name of the Wikibase instance
-wikibase_name = 'opendatakerala.cloud'
+wikibase_name = 'OpenDataKerala'
 
 # URL of the main page of the Wikibase instance
 wikibase_main_page = 'https://opendatakerala.wikibase.cloud/wiki/Main_Page'
@@ -19,29 +20,27 @@ wikibase_main_page = 'https://opendatakerala.wikibase.cloud/wiki/Main_Page'
 # Wikibase namespace ID, used to search for items
 # For Wikidata this is 0, but most by default Wikibase uses 120, which is the default Wikibase 'Item:' namespace
 # CHANGE THIS TO 120 if you are adapting this configuration file to another Wikibase
-wikibase_namespace_id = 120 
-# https://shira.wikibase.cloud/wiki/Special:AllPages?from=&to=&namespace=120
+wikibase_namespace_id = 120
 
 # Namespace prefix of Wikibase items (including colon, e.g. 'Item:')
 wikibase_namespace_prefix = 'Item:'
 
-# User agent to connect to the Wikidata APIs
-user_agent = 'OpenRefine-Wikidata reconciliation interface'
+# User agent to connect to the Wikibase APIs
+user_agent = 'OpenRefine-Wikibase reconciliation interface'
 
 # Regexes and group ids to extracts Qids and Pids from URLs
 import re
-q_re = re.compile(r'(<?https?://shira.wikibase.cloud/(entity|wiki)/)?(Q[0-9]+)>?')
+q_re = re.compile(r'(<?https?://opendatakerala.wikibase.cloud/(entity/|wiki/Item:))?(Q[0-9]+)>?')
 q_re_group_id = 3
-p_re = re.compile(r'(<?https?://shira.wikibase.cloud/(entity/|wiki/Property:))?(P[0-9]+)>?')
+p_re = re.compile(r'(<?https?://opendatakerala.wikibase.cloud/(entity/|wiki/Property:))?(P[0-9]+)>?')
 p_re_group_id = 3
 
 # Identifier space and schema space exposed to OpenRefine.
-# This should match the IRI prefixes used in RDF serialization. For instance: https://shira.wikibase.cloud/wiki/Special:EntityData/Q10.rdf 
+# This should match the IRI prefixes used in RDF serialization.
 # Note that you should be careful about using http or https there,
 # because any variation will break comparisons at various places.
 identifier_space = 'https://opendatakerala.wikibase.cloud/entity/'
 schema_space = 'https://opendatakerala.wikibase.cloud/prop/direct/'
-
 
 # Pattern used to form the URL of a Qid.
 # This is only used for viewing so it is fine to use any protocol (therefore, preferably HTTPS if supported)
@@ -57,7 +56,7 @@ avoid_items_of_class = None
 
 # Service name exposed at various places,
 # mainly in the list of reconciliation services of users
-service_name = 'opendatakerala.cloud recon service'
+service_name = 'Reconcile for OpenDataKerala.'
 
 # URL (without the trailing slash) where this server runs
 this_host = 'http://localhost:8000'
@@ -75,11 +74,11 @@ validation_threshold = 95
 redis_uri = 'redis://redis:6379/0?encoding=utf-8'
 
 # Redis prefix to use in front of all keys
-redis_key_prefix = 'wikibaseopenrefine:'
+redis_key_prefix = 'openrefine_wikibase:'
 
 # Headers for the HTTP requests made by the tool
 headers = {
-    'User-Agent':service_name + ' (wikibaseopenrefine reconciliation service)',
+    'User-Agent':service_name + ' (OpenRefine-Wikibase reconciliation service)',
 }
 
 # Previewing settings
@@ -93,45 +92,45 @@ preview_width = 400
 thumbnail_width = 130
 
 # All properties to use to get an image. Set to empty list [] if no image properties are available.
-image_properties = []
+image_properties = [
+]
 
 # URL pattern to retrieve an image from its filename
 image_download_pattern = 'https://upload.wikimedia.org/wikipedia/commons/thumb/%s/%s/%s/%dpx-%s'
 
 # Fallback URL of the image to use when previewing an item with no image
-fallback_image_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Rothschild_miscellany_dancing.jpg/320px-Rothschild_miscellany_dancing.jpg'
+fallback_image_url = 'https://storage.googleapis.com/wikibase-cloud-static/sites/2dd311e13eb0aada8c98db084b90512b/logos/135.png?u=1695641842'
 
 # Alt text of the fallback image
-fallback_image_alt = 'shira.cloud logo'
+fallback_image_alt = 'Wikibase'
 
 # Autodescribe endpoint to use.
 # this is used to generate automatic descriptions from item contents.
 # (disable this with: autodescribe_endpoint = None )
-autodescribe_endpoint = 'https://tools.wmflabs.org/autodesc/'
+autodescribe_endpoint = None
 
 # Property proposal settings
 
 # Default type : entity (Q35120)
 # Set to None if so such item exists.
-default_type_entity = None
+default_type_entity = 'Q1057'
 
 # Property path used to obtain the type of an item
-# https://opendatakerala.wikibase.cloud/wiki/Property:P2
-type_property_path = 'P2' 
+type_property_path = 'P2'
 
 # Property to follow to fetch properties for a given type.
 # Set to None if this is not available
-# property_for_this_type_property = 'P1963'
 property_for_this_type_property = None
 
 # Optional prefix in front of properties in SPARQL-like property paths
-wdt_prefix = 'wdt'
+wdt_prefix = 'wdt:'
 
 # Sparql query used to fetch all the subclasses of a given item.
-# https://shira.wikibase.cloud/wiki/Property:P40
 # The '$qid' string will be replaced by the qid whose children should be fetched.
 sparql_query_to_fetch_subclasses = """
-SELECT ?child WHERE { ?child wdt:P40* wd:$qid }
+PREFIX wd: <https://opendatakerala.wikibase.cloud/entity/>
+PREFIX wdt: <https://opendatakerala.wikibase.cloud/prop/direct/>
+SELECT ?child WHERE { ?child wdt:P2/wdt:P11* wd:$qid }
 """
 
 # Sparql query used to fetch all the properties which store unique identifiers
@@ -150,7 +149,7 @@ SERVICE gas:service {
     gas:program gas:out1 ?depth .
     gas:program gas:maxIterations 10 .
     gas:program gas:maxVisited 100 .
-    gas:program gas:linkType wdt:P40 .
+    gas:program gas:linkType wdt:P11 .
 }
 SERVICE wikibase:label { bd:serviceParam wikibase:language "$lang" }
 ?out wdt:$property_for_this_type ?prop .
@@ -158,3 +157,4 @@ SERVICE wikibase:label { bd:serviceParam wikibase:language "$lang" }
 ORDER BY ?depth
 LIMIT $limit
 """
+
